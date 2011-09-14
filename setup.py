ba4f0125dfc23f352354de80e84506072e2d007d
privezentsev <privezentsev@gmail.com>
2011-09-14 21:30:00 +0000

    setup.py for windows

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 import py2exe
 
 setup(
-   name="maptools",
+    name="maptools",
     version ="0.0.1",
-         windows=[{"script":"addminimap"}],
-        
-    author_email = "privezentsev@gmail.com",
-    packages = ["maptools"],
-    scripts = ["addminimap","splitmap"]    
+    console=[{"script":"addminimap"}],
+       
+    author_email="privezentsev@gmail.com",
+    packages=["maptools"],
+    scripts=["addminimap","splitmap"]    
 )
