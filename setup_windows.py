8c3c2affd3dd41c91734f2fdc1b5cf938647bfd0
privezentsev <privezentsev@privezentsev-desktop.(none)>
2011-09-15 10:33:57 +0400

    add setup_windows.py

---
+++ setup_windows.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+import py2exe
+setup(
+
+    name="maptools",
+    version ="0.0.1",
+    console=[{"script":"addminimap","script":"splitmap"}],
+       
+    author_email="privezentsev@gmail.com",
+    packages=["maptools"],
+    scripts=["addminimap","splitmap"]    
+)
